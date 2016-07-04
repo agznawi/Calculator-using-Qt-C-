48ec79df7b3d008a0b421d363e3e328f2a28fd6f
Abdullah Al-Ghaznawi <agznawi@users.noreply.github.com>
2016-07-04 13:25:21 +0300

    Create interface.h

---
+++ Calculator/interface.h
@@ -0,0 +1,29 @@
+#ifndef INTERFACE_H
+#define INTERFACE_H
+
+
+#include <QWidget>
+#include <QtWidgets>
+
+class Interface : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit Interface(QWidget *parent = 0);
+
+signals:
+
+public slots:
+    void on_equation_returnPressed();
+    void outputError(QString error);
+
+private:
+    void outputResult(QString expr);
+
+    bool furtherErrors;
+    QLineEdit *equation;
+    QLineEdit *result;
+
+};
+
+#endif // INTERFACE_H
